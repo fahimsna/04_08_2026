4111e6f83739f0e5c8d19f60255d89bfe5789eed
Fahim Shahriar Nur <shahriarfahim091@gmail.com>
2026-08-04 13:04:55 +0600

    Added insert enquiry

--- server/App/controllers/web/enquiryController.js
+++ server/App/controllers/web/enquiryController.js
@@ -1,5 +1,25 @@
 let enquiryInsert = (req, res) => {
   // Implementation for inserting enquiry
-  res.send("Enquiry inserted successfully");
+  let { name, email, phone, message } = req.body;
+  let enquiry = new enquiryModel({
+    name,
+    email,
+    phone,
+    message,
+  });
+  enquiry
+    .save()
+    .then((result) => {
+      res.status(201).json({
+        message: "Enquiry created successfully",
+        enquiry: result,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+      });
+    });
 };
+
 module.exports = { enquiryInsert };
