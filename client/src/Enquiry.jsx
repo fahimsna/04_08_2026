3c99310a14f138578d1a8caea211bd81be064dea
Fahim Shahriar Nur <shahriarfahim091@gmail.com>
2026-08-04 15:00:59 +0600

    Fixed bux

--- client/src/Enquiry.jsx
+++ client/src/Enquiry.jsx
@@ -1,28 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { Button, Label, Textarea, TextInput } from "flowbite-react";
 import { EnquiryList } from "./enquiry/EnquiryList";
 
 export default function Enquiry() {
+  let [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    message: "",
+  });
   const saveEnquiry = (e) => {
     e.preventDefault();
 
-    const formData = {
-      name: e.target.name.value,
-      email: e.target.email.value,
-      phone: e.target.phone.value,
-      message: e.target.message.value,
-    };
+    // const formData = {
+    //   name: e.target.name.value,
+    //   email: e.target.email.value,
+    //   phone: e.target.phone.value,
+    //   message: e.target.message.value,
+    // };
 
     axios
       .post("http://localhost:8020/api/website/enquiry/insert", formData)
       .then((res) => {
         console.log(res.data);
         alert("Enquiry saved successfully!");
-      })
-      .catch((err) => {
-        console.log(err);
       });
+    setFormData({
+      name: "",
+      email: "",
+      phone: "",
+      message: "",
+    });
+  };
+  let getValue = (e) => {
+    let inputName = e.target.name;
+    let inputValue = e.target.value;
+    let oldData = { ...formData };
+    oldData[inputName] = inputValue;
+    setFormData(oldData);
   };
 
   return (
@@ -38,6 +54,8 @@ export default function Enquiry() {
               <Label htmlFor="name">Your Name</Label>
               <TextInput
                 type="text"
+                value={formData.name}
+                onChange={getValue}
                 name="name"
                 placeholder="Enter Your Name"
                 required
@@ -48,6 +66,8 @@ export default function Enquiry() {
               <Label htmlFor="email">Your Email</Label>
               <TextInput
                 type="email"
+                value={formData.email}
+                onChange={getValue}
                 name="email"
                 placeholder="Enter Your Email"
                 required
@@ -58,6 +78,8 @@ export default function Enquiry() {
               <Label htmlFor="phone">Your Phone</Label>
               <TextInput
                 type="text"
+                value={formData.phone}
+                onChange={getValue}
                 name="phone"
                 placeholder="Enter Your Phone"
                 required
@@ -68,6 +90,8 @@ export default function Enquiry() {
               <Label htmlFor="message">Your Message</Label>
               <Textarea
                 name="message"
+                onChange={getValue}
+                value={formData.message}
                 placeholder="Enter Your Message..."
                 rows={4}
                 required
