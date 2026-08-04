5f4a6c98d90e576b76c462b490c1fa2fbda898f6
Fahim Shahriar Nur <shahriarfahim091@gmail.com>
2026-08-04 13:00:42 +0600

    Added router & controllers

---
+++ server/App/controllers/web/enquiryController.js
@@ -0,0 +1,5 @@
+let enquiryInsert = (req, res) => {
+  // Implementation for inserting enquiry
+  res.send("Enquiry inserted successfully");
+};
+module.exports = { enquiryInsert };
--- server/index.js
+++ server/index.js
@@ -1,13 +1,19 @@
 let express = require("express");
 let mongoose = require("mongoose");
+const enquiryRouter = require("./App/routes/web/enquiryRoutes");
 require("dotenv").config();
 let app = express();
+
+app.use(express.json());
+//Routes
+app.use("/api/website/enquiry", enquiryRouter);
+
 
 //conect to mongodb
 mongoose
   .connect(process.env.dbURL)
   .then(() => {
-    app.listen(process.env.PORT||3000, () => {
+    app.listen(process.env.PORT || 3000, () => {
       console.log("Server is running on port " + process.env.PORT);
     });
   })
